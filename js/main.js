4748eaa3c9e2ea73da681ff2487d0555ef06ca17
104062127 <bill851027@gmail.com>
2017-07-21 00:50:34 +0800

    fixed image size and improve css

--- js/main.js
+++ js/main.js
@@ -18,19 +18,19 @@ jQuery(function($) {
     function menuToggle() {
         var windowWidth = $(window).width();
 
-        if (windowWidth > 767) {
-            $(window).on('scroll', function() {
-                if ($(window).scrollTop() > 405) {
-                    $('.main-nav').addClass('fixed-menu animated slideInDown');
-                } else {
-                    $('.main-nav').removeClass('fixed-menu animated slideInDown');
-                }
-            });
-        } else {
+        // if (windowWidth > 767) {
+        //     $(window).on('scroll', function() {
+        //         if ($(window).scrollTop() > 405) {
+        //             $('.main-nav').addClass('fixed-menu animated slideInDown');
+        //         } else {
+        //             $('.main-nav').removeClass('fixed-menu animated slideInDown');
+        //         }
+        //     });
+        // } else {
 
-            $('.main-nav').addClass('fixed-menu animated slideInDown');
+        //     $('.main-nav').addClass('fixed-menu animated slideInDown');
 
-        }
+        // }
     }
 
     menuToggle();
